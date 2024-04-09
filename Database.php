58b72aa37a80ce67219c75d33d1027b798aaa045
Yoon <86170304+leeonthe@users.noreply.github.com>
2024-04-08 20:37:06 -0400

    update database.php

---
+++ Database.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'Config.php';
+require_once 'Database.php';
+
+class Database{
+
+    private $dbConnector;
+
+    public function __construct(){
+
+        $username = COnfig::$db["username"];
+        $password = Config::$db["password"];
+        $host = Config::$db["host"];
+        $database = Config::$db["database"];
+        $dsn = "mysql:host=$host;dbname=$database";
+
+        try 
+{
+//  $db = new PDO("mysql:host=$hostname;dbname=db-demo", $username, $password);
+   $db = new PDO($dsn, $username, $password);
+   
+   // dispaly a message to let us know that we are connected to the database 
+   // echo "<p>You are connected to the database -- host=$host</p>";
+}
+catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+{
+   // Call a method from any object, use the object's name followed by -> and then method's name
+   // All exception objects provide a getMessage() method that returns the error message 
+   $error_message = $e->getMessage();        
+   echo "<p>An error occurred while connecting to the database: $error_message </p>";
+}
+catch (Exception $e)       // handle any type of exception
+{
+   $error_message = $e->getMessage();
+   echo "<p>Error message: $error_message </p>";
+}
+    }
+}
